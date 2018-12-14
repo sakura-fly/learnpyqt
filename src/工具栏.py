7c82a1b6a6f19c8dd828c7b664a38c40b97922e5
sakura-fly <sourcesakura@sina.com>
2018-12-14 09:51:15 +0800

    工具栏

--- src/工具栏.py
+++ src/工具栏.py
@@ -13,10 +13,13 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        # 创建一个行为对象，绑定一个标签，一个图标和快捷键
+        # 行为触发时，调用QtGui.QMainWindow的quit方法退出应用
         exitAct = QAction(QIcon("rin.png"), "退出", self)
-        exitAct.setShortcut("Ctrl+Q")
+        # exitAct.setShortcut("Ctrl+Q")
         exitAct.triggered.connect(qApp.quit)
 
+        # 把工具了展示出来
         self.toolbar = self.addToolBar("退出")
         self.toolbar.addAction(exitAct)
 
