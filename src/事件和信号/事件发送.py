69d806fb95055adda380b8228805d8d8b6cb45f0
sakura-fly <sourcesakura@sina.com>
2018-12-17 14:49:27 +0800

    事件发送

--- src/事件和信号/事件发送.py
+++ src/事件和信号/事件发送.py
@@ -1,7 +1,10 @@
 """
 有时候我们会想知道是哪个组件发出了一个信号，PyQt5里的sender()方法能搞定这件事。
+这个例子里有俩按钮，btnClicked()方法决定了是哪个按钮能调用sender()方法。
 """
-from PyQt5.QtWidgets import QMainWindow, QPushButton
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
 
 
 class Example(QMainWindow):
@@ -15,7 +18,26 @@ class Example(QMainWindow):
         btn1 = QPushButton("b1", self)
         btn2 = QPushButton("b2", self)
 
-        btn1.move(30,50)
-        btn2.move(130,50)
+        btn1.move(30, 50)
+        btn2.move(130, 50)
 
-        btn1.clicked()
+        # 俩按钮都邦洞用一个slot
+        btn1.clicked.connect(self.btnClicked)
+        btn2.clicked.connect(self.btnClicked)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('事件发送')
+        self.show()
+
+    # sender()方法的方式决定了事件源。状态栏显示了被电击的按钮
+    def btnClicked(self):
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + " was press")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
